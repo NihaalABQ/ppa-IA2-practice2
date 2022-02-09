82704c906f9035c2bb8c792bc82680c6ecacbfc6
NihaalABQ <97089172+NihaalABQ@users.noreply.github.com>
2022-02-09 09:24:29 +0000

    p5

--- p5original.c
+++ p5original.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
+int input()
+{
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int gcd(int a,int b)
+{
+  int i,n,flag=0;
+  if(a>=b)
+  n=a;
+  else
+  n=b;
+  for(i=n;i>0;i--)
+  {
+    if(a%i==0 && b%i==0)
+    {
+      flag=1;
+      break;
+    }
+  }
+  if(flag==1)
+  return i;
+  else 
+  return 0;
+}
+
+void output(int a, int b, int gcd)
+{
+  printf("%d is hcf for %d and %d",gcd,a,b);
+}
 int main ()
 {
+  int a,b,g;
+  a=input();
+  b=input();
+  g=gcd(a,b);
+  output(a,b,g);
   return 0;
 }
